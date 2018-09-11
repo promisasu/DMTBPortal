1e82efe7dfedf92ae573d9eaed44f6ab57c4687c
Abhishek <abhishek.uaf30@gmail.com>
2018-09-10 21:33:31 -0700

    Hapi Changes : Not able to fetch data - Async call issue

--- controller/handler/dashboard.js
+++ controller/handler/dashboard.js
@@ -17,9 +17,60 @@ const parameterProp = propReader('parameter.properties');
  * @param {Reply} reply - Hapi Reply
  * @returns {View} Rendered page
  */
-function dashboardView (request, reply) {
-    const currentDate = new Date();
+async function dashboardView (request, reply) {
+   // const currentDate = new Date();
+    let temp = await tempFunc(request,reply);
+    console.log(temp);
+    let temp1 = temp_func(request,reply);
+    console.log(temp1);
 
+    // database.sequelize.query(
+    //     queryProp.get('sql.trials')
+    //     ,
+    //     {
+    //         type: database.sequelize.QueryTypes.SELECT,
+    //         replacements: [
+    //             currentDate.toISOString(),
+    //             currentDate.toISOString()
+    //         ]
+    //     }
+    // )
+    //     .then((trials) => {
+    //         // Process data into format expected in view
+    //         const trialData = trials.map(processTrial);
+
+    //         // Display view
+    //         return reply.view('dashboard', {
+    //             title: parameterProp.get('activity.title'),
+    //             user: request.auth.credentials,
+    //             trials: trialData
+    //         });
+    //     })
+    //     .catch((err) => {
+    //         console.log('error', err);
+
+    //         reply
+    //             .view('404', {
+    //                 title: 'Not Found'
+    //             })
+    //             .code(httpNotFound);
+    //     });
+        return reply.view('dashboard', {
+                title: parameterProp.get('activity.title'),
+                user: request.auth.credentials,
+                //trials: trialData
+            }); 
+}
+
+async function temp_func(request,reply){
+
+    return "Hello World";
+}
+
+async function tempFunc(
+    request,reply){
+     const currentDate = new Date();
+   
     database.sequelize.query(
         queryProp.get('sql.trials')
         ,
@@ -34,13 +85,17 @@ function dashboardView (request, reply) {
         .then((trials) => {
             // Process data into format expected in view
             const trialData = trials.map(processTrial);
+            console.log("trials ..." + trials);
+            console.log("-----------------------------");
+            console.log("trialData : " +trialData );
 
+            return trialData;
             // Display view
-            return reply.view('dashboard', {
-                title: parameterProp.get('activity.title'),
-                user: request.auth.credentials,
-                trials: trialData
-            });
+            // return reply.view('dashboard', {
+            //     title: parameterProp.get('activity.title'),
+            //     user: request.auth.credentials,
+            //     trials: trialData
+            // });
         })
         .catch((err) => {
             console.log('error', err);
@@ -51,11 +106,5 @@ function dashboardView (request, reply) {
                 })
                 .code(httpNotFound);
         });
-        return reply.view('dashboard', {
-                title: parameterProp.get('activity.title'),
-                user: request.auth.credentials,
-                //trials: trialData
-            }); 
 }
-
 module.exports = dashboardView;
